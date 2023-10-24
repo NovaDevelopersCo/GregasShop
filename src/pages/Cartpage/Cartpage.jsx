8f3d303c71f4f3fa4005a2c6f1a5db2b94d7ea98
vdmitx <129660122+vdmitx@users.noreply.github.com>
2023-10-24 22:53:13 +0300

    cart page scss

--- src/pages/Cartpage/Cartpage.jsx
+++ src/pages/Cartpage/Cartpage.jsx
@@ -7,15 +7,36 @@ export const Cartpage = () => {
   return (
     <div className={styles.wrapper}>
       <h1>Корзина</h1>
+      <div className='cart-name'>Корзина</div>
       <h4>Главная - Корзина</h4>
-      <>hi</>
-      <div className={styles.basketbox}>
+      <div className='box'>
         <div className='box-line'>вапджыдваоп</div>
         <div className='box-section'>
           <div className='box-img'>вапвап</div>
           <div className='box-name'>ываыва</div>
           <div className='box-pricePerOne'>ываываы</div>
           <div className='box-quantity'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
+          <div className='box-totalPrice'>ываыва</div>
           <div className='box-totalPrice'>ываыва</div>
         </div>
       </div>
