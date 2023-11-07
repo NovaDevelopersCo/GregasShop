426f8acf8395a5a19a2ccd35f8c7be0473883091
vdmitx <129660122+vdmitx@users.noreply.github.com>
2023-11-08 00:41:10 +0300

    Personalacc module scss

--- src/pages/Personalacc/Personalacc.jsx
+++ src/pages/Personalacc/Personalacc.jsx
@@ -1,22 +1,10 @@
-    
-import './Personalacc.scss';
+import styles from './Personalacc.module.scss'; // модульность css
 
 
 export const Personalacc = () => {
   return (
-    <div className='wrapper'>\
-      <h1>Personal account</h1>
-      <h4>Главная - Корзина</h4>
-      <div className='box'>
-        <div className='box-line'></div>
-        <div className='box-section'>
-          <div className='box-img'></div>
-          <div className='box-name'></div>
-          <div className='box-pricePerOne'></div>
-          <div className='box-quantity'></div>
-          <div className='box-totalPrice'></div>
-        </div>
-      </div>
+    <div className={styles.wrapper}>
+      <h3>Personal account</h3>
     </div>
   );
 };
