5fc7cbeefa68de4be8a11f3e485eb7b92255e8c5
Alex <honkgoose0@gmail.com>
2023-11-07 19:18:00 +0300

    slider + photo button commit again

--- src/pages/CatalogPage/CatalogPage.js
+++ src/pages/CatalogPage/CatalogPage.js
@@ -2,17 +2,28 @@ import React from 'react';
 import './CatalogPage.scss';
 import { CatalogData } from './CatalogData';
 import { Subcategories } from './SubCategories';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export const CatalogPage = () => {
   const subcategoryStart = [0,6,11,14,15,25,26]; // Создаем массив, который содержит количество подкатегорий для каждой категории
   const subcategoryCounts = [6,11,14,23,25,30]; // Замените на фактические значения
+//   }
 
+// export default function BreadCrumbs() {
+//   const location = useLocation()
+
+//   let currentLink = ''
+
+//   const BreadCrumbs = location.pathname.split('/')
+//   .filter(crumb => crumb !== '')
+//   .map(crumb => {
+//     currentLink =+ `/${crumb}`
+//   })
 
   return (
     <div>
       <h2>Каталог</h2>
-      <p className="BreadCrumbs"> Главная - Каталог </p>
+      <div className="BreadCrumbs"> Главная - Каталог </div>
       <br></br>
       <div className="CategoryBoxWrapper">
         {CatalogData.map((obj, index) => {
@@ -36,3 +47,4 @@ export const CatalogPage = () => {
     </div>
   );
 };
+
