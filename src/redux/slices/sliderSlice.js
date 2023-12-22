ff4cbc588d4a2a76a02c80b7cf46f99d7d6b3f5d
Kycedron <138807475+Kycedron@users.noreply.github.com>
2023-12-22 20:34:07 +0300

    add Slider on Backend

---
+++ src/redux/slices/sliderSlice.js
@@ -0,0 +1,45 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from '../../axios';
+
+export const fetchSlider = createAsyncThunk('slider/fetchSlider', async (params) => {
+
+  const { data } = await axios.get(
+    `/slider`
+  );
+  return data;
+});
+const initialState = {
+  slider: [],
+  status: {
+    all: 'loading', // Общий статус
+
+  },
+};
+
+const sliderSlice = createSlice({
+  name: 'slider',
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchSlider.pending, (state, action) => {
+        state.status.all = 'loading'
+      })
+      .addCase(fetchSlider.fulfilled, (state, action) => {
+        console.log('Fetched news data:', action.payload.data);
+        state.status.all = 'success';
+        if (action.meta.arg.itemCategory === 'all') {
+          state.slider = action.payload;
+          state.status.all = 'success';
+        }
+      })
+      .addCase(fetchSlider.rejected, (state, action) => {
+        state.status.all = 'error';
+      });
+  },
+});
+
+
+export const { setSlider } = sliderSlice.actions;
+export const selectSlider = (state) => state.sliderSlice;
+export default sliderSlice.reducer;
