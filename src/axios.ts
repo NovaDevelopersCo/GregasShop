964a6a3ca9cd2163c78a1a292c4a4fae361719a9
merdernoty <133769530+merdernoty@users.noreply.github.com>
2023-12-24 20:22:46 +0300

    fix

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'backend-kmay.vercel.app'
+    baseURL: 'https://backend-kmay.vercel.app'
     // baseURL: 'http://localhost:4444/'
 })
 
