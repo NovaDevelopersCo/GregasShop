cbca6e8e5b46d3a3aee3c0e3452a5f372c52c7b1
Roma8642 <141335182+Roma8642@users.noreply.github.com>
2024-01-02 18:29:41 +0200

    small fixs

--- src/layouts/BaseLayout/BaseLayout.tsx
+++ src/layouts/BaseLayout/BaseLayout.tsx
@@ -1,16 +1,18 @@
-import { Header } from '../../Components';
+import React from 'react';
+import { Header, Footer } from '../../Components';
 import { Outlet } from 'react-router-dom';
-import { Footer } from '../../Components';
 import './BaseLayout.scss';
 import { SearchProvider } from '../../hooks/context/SearchContext';
+import ScrollToTop from '../../hooks/ScrollToTop';
 
 export const BaseLayout = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <SearchProvider>
         <div className='HeaderWrapper'>
           <Header />
         </div>
+        <ScrollToTop />
         <Outlet />
         <Footer />
       </SearchProvider>
